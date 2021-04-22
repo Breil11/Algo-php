5a25e01e3582e0e948a8a9602b2c2dbc5d71c63a
Breil11 <breilkom@gmail.com>
2021-04-22 14:21:09 +0200

    Add tp8

---
+++ index8.php
@@ -0,0 +1,16 @@
+<?php
+/*Verification d'un nombre compris entre 10 et 20 et affiche le message correspondant*/
+
+$nombre = 7;
+
+
+
+if($nombre > 20){
+    echo("Plus petit !");}
+    
+else{
+    echo("Plus grand!");
+}
+
+
+?>
