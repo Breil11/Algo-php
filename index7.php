8ae7a950cc66f894ed7f3cf161c8a45189a072ba
Breil11 <breilkom@gmail.com>
2021-04-22 14:20:17 +0200

    Add tp7

---
+++ index7.php
@@ -0,0 +1,13 @@
+<?php
+/*Verification d'un nombre compris entre 1 et 10*/
+
+$nombre = 7;
+
+if($nombre > 1 && $nombre < 10){
+    echo("Ce nombre convient!");
+}
+else{
+    echo("Ce nombre  ne convient pas !");
+}
+
+?>
